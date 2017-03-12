9ca703c3ebbb9233811026cfcae1d9cb6a0678a0
George35mk <giorgoshatoutsidis@gmail.com>
2017-03-13 00:05:11 +0200

    cleaning index.js

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 // const electron = require('electron');
 // const app = electron.app;
 // const BrowserWindow = electron.BrowserWindow;
-// // const autoUpdater = require("electron-updater").autoUpdater
 
-// // console.log(autoUpdater);
 
 // const feedURL = 'https://github.com/George35mk/plx17/releases/download/1.0.1/v-1.0.1.zip';
-
-
-
-// var log = require('electron-log');
-
 
 const appVersion = require('./package.json').version;
 const os = require('os').platform();
@@ -19,4 +12,3 @@ const os = require('os').platform();
 console.log('The OS is : ' + os); // win32
 console.log('The App Version is : ' + appVersion); // 1.0.0
 
-const autoUpdater = require("electron-updater").autoUpdater;
