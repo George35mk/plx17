20f0512302108452b552162f69bafe222baca5ae
George35mk <giorgoshatoutsidis@gmail.com>
2017-03-17 13:47:33 +0200

    controller for selectors on select page test gitignore file

--- Pages/select.js
+++ Pages/select.js
@@ -1,4 +1,5 @@
 // +++++++++++++ System types +++++++++++++
+
 $("#anoigomena").click(function () {
     $(".system_type").hide();
     $("#anoigomena_content").fadeIn();
@@ -49,3 +50,75 @@ $("#eidikes_kataskeves").click(function () {
     $(".sys_types").removeClass("active");
     $("#eidikes_kataskeves").addClass("active");
 });
+
+
+// ama dialeksei seira dixnei tin ekatoste seira pou dialekse
+function controler_select_anoigomena_seira(_id){
+    // alert(_id);
+}
+
+// ama dialeksi portes kanei show tis ports kai outo katheksis
+function controler_select_anoigomena_type(_id) {
+
+    let val = $(`#${_id}`).val();
+    // alert(s);
+
+
+    switch (val) {
+
+        case "portes":
+            $(".anoigomenes_kataskeves").hide();
+            $("#portes").fadeIn();
+            break;
+
+        case "parathira":
+            $(".anoigomenes_kataskeves").hide();
+            $("#parathira").fadeIn();
+            break;
+
+        case "mpalkonoportes":
+            $(".anoigomenes_kataskeves").hide();
+            $("#mpalkonoportes").fadeIn();
+            break;
+
+        case "stathera":
+            $(".anoigomenes_kataskeves").hide();
+            $("#stathera").fadeIn();
+            break;
+
+        default:
+            alert('Wtf you select');
+            break;
+    }
+
+
+
+}
+
+function controler_select_seiromena_type(_id) {
+
+    let val = $(`#${_id}`).val();
+    // alert(s);
+
+
+    switch (val) {
+
+        case "epalila":
+            $(".seiromenes_kataskeves").hide();
+            $("#epalila").fadeIn();
+            break;
+
+        case "xonefta":
+            $(".seiromenes_kataskeves").hide();
+            $("#xonefta").fadeIn();
+            break;
+
+
+        default:
+            alert('Wtf you select');
+            break;
+    }
+
+
+
+}
